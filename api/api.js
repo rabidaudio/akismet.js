6ab8dc47d2a1768f31f8b2aa79b22971a58c96bc
Cédric Belin <cedric@belin.io>
2014-09-18 12:25:33 +0200

    Updated the API reference to v0.2.1

--- api/api.js
+++ api/api.js
@@ -1,26 +1,40 @@
 YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
-        "Author",
-        "Client",
-        "Comment",
-        "CommentType",
-        "EndPoints"
+        "akismet.Author",
+        "akismet.Client",
+        "akismet.Comment",
+        "akismet.CommentType",
+        "akismet.EndPoints",
+        "akismet.HTTPHeaders",
+        "akismet.Server"
     ],
     "modules": [
         "client",
-        "comment"
+        "comment",
+        "enums",
+        "server"
     ],
     "allModules": [
         {
             "displayName": "client",
             "name": "client",
-            "description": "Contains classes that provide a simple programming interface for querying [Akismet](https://akismet.com) service."
+            "description": "Contains classes that provide a simple programming interface for querying the [Akismet](https://akismet.com) service."
         },
         {
             "displayName": "comment",
             "name": "comment",
-            "description": "Provides classes describing a comment."
+            "description": "Provides classes describing a comment and its author."
+        },
+        {
+            "displayName": "enums",
+            "name": "enums",
+            "description": "Contains enumerations that define commonly-used values."
+        },
+        {
+            "displayName": "server",
+            "name": "server",
+            "description": "Provides an HTTP server for relaying queries from clients to the [Akismet](https://akismet.com) service."
         }
     ]
 } };
